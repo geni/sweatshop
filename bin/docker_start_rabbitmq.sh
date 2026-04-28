bac89af79851d60c9a66d255edc6592f395bf612
Scott Steadman <scott.steadman@geni.com>
2026-04-28 15:16:40 -0700

    stop using docker compose plugin
      Jenkins doesn't have the docker compose plugin installed.

--- bin/docker_start_rabbitmq.sh
+++ bin/docker_start_rabbitmq.sh
@@ -12,7 +12,17 @@ do
 	fi
 done
 
-docker compose -f docker/docker-compose.yml up -d
+docker volume create rabbitmq_data
+
+docker run -d --rm \
+  --name rabbitmq \
+  -p 5672:5672 \
+  -p 15672:15672 \
+  -e RABBITMQ_DEFAULT_USER=guest \
+  -e RABBITMQ_DEFAULT_PASS=guest \
+  -v rabbitmq_data:/var/lib/rabbitmq \
+  rabbitmq:management
+
 docker exec rabbitmq bash -c 'rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@${HOSTNAME}.pid'
 docker exec rabbitmq rabbitmqctl add_vhost two
 docker exec rabbitmq rabbitmqctl set_permissions -p two guest ".*" ".*" ".*"
--- bin/docker_stop_rabbitmq.sh
+++ bin/docker_stop_rabbitmq.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker compose -f docker/docker-compose.yml down
+docker stop rabbitmq
+#docker rm rabbitmq
